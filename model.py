9dd7db51dd70818a3a4698407b9b0462bcda223f
Aleksandr Dvorakovskiy <aleksandr.dvorakovskiy@mytona.com>
2022-02-20 01:23:06 +0900

    train model

--- model.py
+++ model.py
@@ -4,13 +4,22 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn.metrics as metrics
+import seaborn as sns
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.layers import BatchNormalization, Conv2D, MaxPool2D, Dropout, Flatten, Dense
+from keras.models import Sequential
+from keras.utils.np_utils import to_categorical
+from sklearn.metrics import classification_report, roc_curve, auc
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras.preprocessing.image import load_img, img_to_array
+from sklearn.preprocessing import label_binarize
+from tensorflow import keras
 
 img_size = (224,224)
+
 dir_name = 'COVID-19_Radiography_Dataset/COVID'
 img_list = glob.glob(dir_name + '/*')
 list_covid = []
@@ -24,7 +33,6 @@ df_covid=pd.DataFrame(list_covid2)
 df_covid['label'] = np.full(df_covid.shape[0],2)
 print(df_covid.shape)
 
-img_size = (224,224)
 dir_name2 = 'COVID-19_Radiography_Dataset/Normal'
 img_list2 = glob.glob(dir_name2 + '/*')
 list_normal = []
@@ -39,7 +47,6 @@ df_normal['label'] = np.full(df_normal.shape[0],0)
 print(df_normal.shape)
 
 
-img_size = (224,224)
 dir_name3 = 'COVID-19_Radiography_Dataset/Viral Pneumonia'
 img_list3 = glob.glob(dir_name3 + '/*')
 list_others = []
@@ -52,3 +59,102 @@ list_others2 = list_others.reshape(-1,50176)
 df_others=pd.DataFrame(list_others2)
 df_others['label'] = np.full(df_others.shape[0],1)
 print(df_others.shape)
+
+Df = pd.concat([df_covid, df_normal, df_others], ignore_index=True)
+x_train, x_test, y_train, y_test = train_test_split(Df.iloc[:,0:-1], Df.iloc[:,-1], test_size=0.20, random_state=None)
+
+X_train = x_train.values.reshape(-1,224,224,1)
+X_test = x_test.values.reshape(-1,224,224,1)
+Y_train = keras.utils.to_categorical(y_train)
+Y_test = keras.utils.to_categorical(y_test)
+
+model = Sequential()
+model.add(BatchNormalization(input_shape=(224,224,1)))
+model.add(Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.35))
+
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.35))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.35))
+model.add(Dense(3, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model_chkpt = ModelCheckpoint('model.h5', monitor='accuracy')
+early_stopping = EarlyStopping(monitor='loss', restore_best_weights=False,patience=10)
+history = model.fit(X_train, Y_train, validation_split=0.20, epochs=20, batch_size=32, shuffle=True, callbacks=[model_chkpt, early_stopping])
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 10))
+t = f.suptitle('Fast-CovNet Performance', fontsize=16,fontweight='bold')
+f.subplots_adjust(top=0.9, wspace=0.1)
+
+max_epoch = len(history.history['accuracy'])+1
+epoch_list = list(range(1,max_epoch))
+ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(1, max_epoch, 1))
+ax1.set_ylabel('Accuracy Value',fontsize=14,fontweight='bold')
+ax1.set_xlabel('Epoch',fontsize=14,fontweight='bold')
+ax1.set_title('Accuracy', fontsize=14,fontweight='bold')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(1, max_epoch, 1))
+ax2.set_ylabel('Loss Value',fontsize=14,fontweight='bold')
+ax2.set_xlabel('Epoch',fontsize=14,fontweight='bold')
+ax2.set_title('Loss',fontsize=14,fontweight='bold')
+l2 = ax2.legend(loc="best")
+plt.figure()
+ax = plt.subplot()
+
+ax.set_title('Confusion Matrix')
+pred = model.predict_classes(X_test)
+Y_TEST = np.argmax(Y_test, axis =1)
+cm = metrics.confusion_matrix(Y_TEST,pred)
+classes=['normal', 'other pneumonia', 'covid19']
+sns.heatmap(cm, annot=True,xticklabels=classes, yticklabels=classes,cmap='Blues')
+
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.show
+print(classification_report(Y_TEST, pred, digits = 5))
+print('normal = 0 , other pneumonia = 1, covid = 2')
+
+#f = plt.subplots(figsize=(5, 5))
+PRED = to_categorical(pred)
+y = Df['label'].values
+# Binarize the output
+y = label_binarize(y, classes=[0,1,2])
+n_classes = y.shape[1]
+
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+       fpr[i], tpr[i], _ = roc_curve(Y_test[:,i], PRED[:,i])
+       roc_auc[i] = auc(fpr[i], tpr[i])
+       colors = ['blue', 'red', 'green']
+cls = {0:'normal', 1:'other pneumonia', 2:'covid'}
+for i, color ,c in zip(range(n_classes), colors, cls.values()):
+    plt.plot(fpr[i], tpr[i], color=color, lw=0.5,
+             label='ROC curve of '+c+ '(AUC = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+plt.plot([0, 1], [0, 1], 'k--',linestyle='--')
+plt.xlim([-0.05, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC for multi-class data')
+plt.legend(loc="lower right")
+plt.show()
+
+model.save('MODEL_COVID19')
